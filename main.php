72597d2ce329298b914f1ef63acd4716fa0abcac
Dennis1092 <36614258+Dennis1092@users.noreply.github.com>
2018-10-16 17:22:26 -0500

    d

---
+++ main.php
@@ -0,0 +1,130 @@
+<style>img[alt*="www.000webhost.com"] {
+    display: none;} 
+img[alt="www.000webhost.com"]{display:none;}
+</style>
+
+
+<!DOCTYPE html>
+
+
+<html lang="en-US">
+<html lang="en-US">
+  <head profile="http://www.w3.org/2005/10/profile">
+</head>
+</html>
+</html>
+
+   
+<html>
+<head>
+<body>
+<title>Free Web Proxy - MikeLime</title>
+  <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+<meta name="generator" content="MikeLime's Web Proxy">
+<meta name="version" content="<?=$version;?>">
+<meta name="description" content="With MikeLime's free web proxy, you can browse the internet freely without your school being able to see what you're doing! This web proxy will even unblock websites, and we have a wide selection of unblocked games.">
+<link rel="canonical" href="https://www.mikelime.ml">
+<meta charset="utf-8">
+<meta name="application-name" content="MikeLime's Web Proxy">
+<meta name="KeyWords" content="unblocked games, web proxy, how to unblock websites, website unblocker, unblock games, proxysite">
+<meta name="google-site-verification" content="Dq5W8bNH90DDy-tTImw5oFxSnS01ZYwENdl9Xp12WMo" />
+ <meta http-Equiv="Cache-Control" Content="no-cache" />
+ <meta http-Equiv="Pragma" Content="no-cache" />
+ <meta http-Equiv="Expires" Content="0" />
+
+
+
+<style type="text/css">
+html body {
+font-family: Arial,Helvetica,sans-serif;
+font-size: 12px;
+}
+
+#container {
+width:500px;
+margin:0 auto;
+margin-top:150px;
+}
+
+#error {
+color:red;
+font-weight:bold;
+}
+
+#frm {
+padding:10px 15px;
+background-color:#282828;
+
+border:1px solid #4dff21;
+
+-webkit-border-radius: 8px;
+-moz-border-radius: 8px;
+border-radius: 8px;
+}
+
+#footer {
+text-align:center;
+font-size:10px;
+margin-top:35px;
+clear:both;
+}
+</style>
+
+<style>.button-one {
+  background: #ffffff;
+}
+.button-two {
+  background: #00d80a;
+}
+.button-three {
+  background: #ffffff;
+}
+.button-four {
+  background: #00d80a;
+}
+.button-five {
+  background: #ffffff;
+}
+.button-one,
+.button-two,
+.button-three,
+.button-four,
+.button-five {
+  color: #282828;
+  display: inline-block;
+  border-radius: 0;
+  padding: 10px 18px;
+  text-transform: uppercase;
+  font-weight: 700;
+  letter-spacing: 1px;
+  -moz-transition: all 0.2s;
+  -webkit-transition: all 0.2s;
+  transition: all 0.2s;
+}
+.button-one:hover,
+.button-two:hover,
+.button-three:hover,
+.button-four:hover,
+.button-five:hover {
+  background: #fff;
+}
+</style>
+
+</head>
+
+        
+
+
+     
+<iframe allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" oallowfullscreen="true" msallowfullscreen="true" src="https://agile-brushlands-89379.herokuapp.com/"  style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:11;"> <!--top was 25!-->
+</iframe>
+
+
+
+
+
+
+
+
+</body>
+</html>
