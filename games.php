ade1c6279f10e6a359840734e1c46b86dd8c1546
Dennis1092 <36614258+Dennis1092@users.noreply.github.com>
2018-10-16 15:50:32 -0500

    bet

---
+++ games.php
@@ -0,0 +1,410 @@
+﻿
+
+  <style>img[alt*="www.000webhost.com"] {
+      display: none;}
+  img[alt="www.000webhost.com"]{display:none;}
+  </style>
+
+
+  <!DOCTYPE html>
+  <html lang="en-US">
+  <html lang="en-US">
+    <head profile="http://www.w3.org/2005/10/profile">
+    <link rel="icon"
+          href="https://ssl.gstatic.com/docs/doclist/images/infinite_arrow_favicon_4.ico">
+  </head>
+  </html>
+  </html>
+<html>
+<head>
+  <body style="background-color:#282828;">
+  <title>My Drive - Google Drive</title>
+
+  <style type="text/css">
+html body {
+	font-family: Arial,Helvetica,sans-serif;
+	font-size: 12px;
+}
+
+#container {
+	width:500px;
+	margin:0 auto;
+	margin-top:150px;
+}
+
+#error {
+	color:red;
+	font-weight:bold;
+}
+
+#frm {
+	padding:10px 15px;
+	background-color:#282828;
+
+	border:1px solid #4dff21;
+
+	-webkit-border-radius: 8px;
+	-moz-border-radius: 8px;
+	border-radius: 8px;
+}
+
+#footer {
+	text-align:center;
+	font-size:10px;
+	margin-top:35px;
+	clear:both;
+}
+</style>
+
+<style>.button-one {
+    background: #ffffff;
+}
+.button-two {
+    background: #00d80a;
+}
+.button-three {
+    background: #ffffff;
+}
+.button-four {
+    background: #00d80a;
+}
+.button-five {
+    background: #ffffff;
+}
+.button-one,
+.button-two,
+.button-three,
+.button-four,
+.button-five {
+    color: #1d1d1d;
+    display: inline-block;
+    border-radius: 0;
+    padding: 10px 18px;
+    text-transform: uppercase;
+    font-weight: 700;
+    letter-spacing: 1px;
+    -moz-transition: all 0.2s;
+    -webkit-transition: all 0.2s;
+    transition: all 0.2s;
+}
+.button-one:hover,
+.button-two:hover,
+.button-three:hover,
+.button-four:hover,
+.button-five:hover {
+    background: #fff;
+}
+</style>
+
+
+
+<style>
+@charset "UTF-8";
+@import url("//fonts.googleapis.com/css?family=Pacifico&text=Pure");
+@import url("//fonts.googleapis.com/css?family=Roboto:700&text=css");
+@import url("//fonts.googleapis.com/css?family=Kaushan+Script&text=!");
+body {
+  min-height: 450px;
+  height: 100vh;
+  margin: 0;
+  background: radial-gradient(circle, #00d80a, #1f9628, #103818, #000);
+  background-attachment: fixed;
+}
+
+.stage {
+  height: 300px;
+  width: 500px;
+  margin: auto;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  perspective: 9999px;
+  transform-style: preserve-3d;
+}
+
+.layer {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  transform-style: preserve-3d;
+  animation: ಠ_ಠ 5s infinite alternate ease-in-out -7.5s;
+  animation-fill-mode: forwards;
+  transform: rotateY(40deg) rotateX(33deg) translateZ(0);
+}
+
+.layer:after {
+  font: 150px/0.65 'Pacifico', 'Kaushan Script', Futura, 'Roboto', 'Trebuchet MS', Helvetica, sans-serif;
+  content: 'MikeLimes\A    Games!';
+  white-space: pre;
+  text-align: center;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 50px;
+  color: whitesmoke;
+  letter-spacing: -2px;
+  text-shadow: 4px 0 10px rgba(0, 0, 0, 0.13);
+}
+
+.layer:nth-child(1):after {
+  transform: translateZ(0px);
+}
+
+.layer:nth-child(2):after {
+  transform: translateZ(-1.5px);
+}
+
+.layer:nth-child(3):after {
+  transform: translateZ(-3px);
+}
+
+.layer:nth-child(4):after {
+  transform: translateZ(-4.5px);
+}
+
+.layer:nth-child(5):after {
+  transform: translateZ(-6px);
+}
+
+.layer:nth-child(6):after {
+  transform: translateZ(-7.5px);
+}
+
+.layer:nth-child(7):after {
+  transform: translateZ(-9px);
+}
+
+.layer:nth-child(8):after {
+  transform: translateZ(-10.5px);
+}
+
+.layer:nth-child(9):after {
+  transform: translateZ(-12px);
+}
+
+.layer:nth-child(10):after {
+  transform: translateZ(-13.5px);
+}
+
+.layer:nth-child(11):after {
+  transform: translateZ(-15px);
+}
+
+.layer:nth-child(12):after {
+  transform: translateZ(-16.5px);
+}
+
+.layer:nth-child(13):after {
+  transform: translateZ(-18px);
+}
+
+.layer:nth-child(14):after {
+  transform: translateZ(-19.5px);
+}
+
+.layer:nth-child(15):after {
+  transform: translateZ(-21px);
+}
+
+.layer:nth-child(16):after {
+  transform: translateZ(-22.5px);
+}
+
+.layer:nth-child(17):after {
+  transform: translateZ(-24px);
+}
+
+.layer:nth-child(18):after {
+  transform: translateZ(-25.5px);
+}
+
+.layer:nth-child(19):after {
+  transform: translateZ(-27px);
+}
+
+.layer:nth-child(20):after {
+  transform: translateZ(-28.5px);
+}
+
+.layer:nth-child(n+10):after {
+  -webkit-text-stroke: 3px rgba(0, 0, 0, 0.25);
+}
+
+.layer:nth-child(n+11):after {
+  -webkit-text-stroke: 15px #2cf200;
+  text-shadow: 6px 0 6px #006b0e, 5px 5px 5px #005001, 0 6px 6px #006b0e;
+}
+
+.layer:nth-child(n+12):after {
+  -webkit-text-stroke: 15px #00d80a;
+}
+
+.layer:last-child:after {
+  -webkit-text-stroke: 17px rgba(0, 0, 0, 0.1);
+}
+
+.layer:first-child:after {
+  color: #fff;
+  text-shadow: none;
+}
+
+@keyframes ಠ_ಠ {
+  100% {
+    transform: rotateY(-40deg) rotateX(-43deg);
+  }
+}
+</style>
+
+<!-- PopAds.net Popunder Code for www.mikelime.ml -->
+<script type="text/javascript" data-cfasync="false">
+/*<![CDATA[/* */
+  var _pop = _pop || [];
+  _pop.push(['siteId', 2845192]);
+  _pop.push(['minBid', 0]);
+  _pop.push(['popundersPerIP', 0]);
+  _pop.push(['delayBetween', 0]);
+  _pop.push(['default', false]);
+  _pop.push(['defaultPerDay', 0]);
+  _pop.push(['topmostLayer', false]);
+  (function() {
+    var pa = document.createElement('script'); pa.type = 'text/javascript'; pa.async = true;
+    var s = document.getElementsByTagName('script')[0];
+    pa.src = '//c1.popads.net/pop.js';
+    pa.onerror = function() {
+      var sa = document.createElement('script'); sa.type = 'text/javascript'; sa.async = true;
+      sa.src = '//c2.popads.net/pop.js';
+      s.parentNode.insertBefore(sa, s);
+    };
+    s.parentNode.insertBefore(pa, s);
+  })();
+/*]]>/* */
+</script>
+<!-- PopAds.net Popunder Code End -->
+</head>
+
+<a class="button-one" title="proxy" href="/index.php" style="text-decoration:none">Proxy</a><a class="button-two" title="games" href="/games.html" style="text-decoration:none">Games</a><a class="button-three" title="movies" href="/movies.html" style="text-decoration:none">Movies</a><a class="button-four" title="chatbox" href="/chatbox.html" style="text-decoration:none">Chatbox</a>
+
+
+
+
+<div class="stage">
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+</div>
+
+
+
+
+
+
+
+
+
+  <br>
+  </br>
+
+  <div style="text-align:middle;">
+    <a href="/games/2048.html" style="color:#00d80a">2048</a>
+  </div>
+
+
+  <div style="text-align:middle;"><div style="height: 8px; overflow: hidden;"></div>
+  <a href="/games/ducklife.html" style="color:#00d80a">Duck Life 4</a>
+
+  <div style="text-align:middle;"><div style="height: 8px; overflow: hidden;"></div>
+  <a href="/games/bindingofisaac.html" style="color:#00d80a">The Binding of Isaac</a>
+
+  <div style="text-align:middle;"><div style="height: 8px; overflow: hidden;"></div>
+  <a href="/games/awesomecars.html" style="color:#00d80a">Awesome Cars</a>
+
+
+  <div style="text-align:middle;"><div style="height: 8px; overflow: hidden;"></div>
+  <a href="/games/smash.html" style="color:#00d80a" >Super Smash Flash</a>
+
+  <div style="text-align:middle;"><div style="height: 8px; overflow: hidden;"></div>
+  <a href="/games/ssf2.html" style="color:#00d80a">Super Smash Flash 2</a>
+
+  <div style="text-align:middle;"><div style="height: 8px; overflow: hidden;"></div>
+  <a href="/games/happywheels.html" style="color:#00d80a" >Happy Wheels</a>
+
+  <div style="text-align:middle;"><div style="height: 8px; overflow: hidden;"></div>
+  <a href="/games/gunmayhem.html" style="color:#00d80a" >Gun Mayhem</a>
+
+  <div style="text-align:middle;"><div style="height: 8px; overflow: hidden;"></div>
+  <a href="/games/gunmayhem2.html" style="color:#00d80a" >Gun Mayhem 2</a>
+
+  <div style="text-align:middle;"><div style="height: 8px; overflow: hidden;"></div>
+  <a href="/games/gunmayhemredux.html" style="color:#00d80a" >Gun Mayhem Redux</a>
+
+  <div style="text-align:middle;"><div style="height: 8px; overflow: hidden;"></div>
+  <a href="/games/sportsheadsfootball.html" style="color:#00d80a" >Sports Heads - Football</a>
+
+  <div style="text-align:middle;"><div style="height: 8px; overflow: hidden;"></div>
+  <a href="/games/sportsheadsbasketball.html" style="color:#00d80a" >Sports Heads - Basketball</a>
+
+  <div style="text-align:middle;"><div style="height: 8px; overflow: hidden;"></div>
+  <a href="/games/sportsheadshockey.html" style="color:#00d80a" >Sports Heads - Hockey</a>
+
+  <div style="text-align:middle;"><div style="height: 8px; overflow: hidden;"></div>
+  <a href="/games/sportsheadstennis.html" style="color:#00d80a" >Sports Heads - Tennis</a>
+
+  <div style="text-align:middle;"><div style="height: 8px; overflow: hidden;"></div>
+  <a href="/games/gta.html" style="color:#00d80a">Grand Theft Auto</a>
+
+  <div style="text-align:middle;"><div style="height: 8px; overflow: hidden;"></div>
+  <a href="/games/acegangster.html" style="color:#00d80a" >Ace Gangster Taxi</a>
+
+  <div style="text-align:middle;"><div style="height: 8px; overflow: hidden;"></div>
+  <a href="/games/acegangster1.html" style="color:#00d80a" >Ace Gangster</a>
+
+  <div style="text-align:middle;"><div style="height: 8px; overflow: hidden;"></div>
+  <a href="/games/clickerheroes.html" style="color:#00d80a" >Clicker Heroes</a>
+
+  <div style="text-align:middle;"><div style="height: 8px; overflow: hidden;"></div>
+  <a href="/games/superfighters.html" style="color:#00d80a" >Super Fighters</a>
+
+  <div style="text-align:middle;"><div style="height: 8px; overflow: hidden;"></div>
+  <a href="/games/portal.html" style="color:#00d80a" >Portal</a>
+
+  <div style="text-align:middle;"><div style="height: 8px; overflow: hidden;"></div>
+  <a href="/games/johnnyupgrade.html" style="color:#00d80a">Johnny Upgrade</a>
+
+  <div style="text-align:middle;"><div style="height: 8px; overflow: hidden;"></div>
+  <a href="/games/3dcar.html" style="color:#00d80a">3D Car Racing</a>
+
+  <div style="text-align:middle;"><div style="height: 8px; overflow: hidden;"></div>
+  <a href="/games/truckloader4.html" style="color:#00d80a">Truck Loader 4</a>
+  
+    <div style="text-align:middle;"><div style="height: 8px; overflow: hidden;"></div>
+  <a href="/games/snake.html" style="color:#00d80a">Snake</a>
+
+
+
+
+
+  <script type="text/javascript"> var infolinks_pid = 3125271; var infolinks_wsid = 0; </script> <script type="text/javascript" src="//resources.infolinks.com/js/infolinks_main.js"></script>
+
+  </div>
+
+  </html>
+</body>
---
+++ templates/main.php
@@ -0,0 +1,186 @@
+<style>img[alt*="www.000webhost.com"] {
+    display: none;} 
+img[alt="www.000webhost.com"]{display:none;}
+</style>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<body>
+
+
+
+
+
+<body>
+<title>My Drive - Google Drive</title>
+  <link rel="shortcut icon" type="image/x-icon" href="mikelime.ico" />
+<meta name="generator" content="MikeLime's Web Proxy">
+<meta name="version" content="<?=$version;?>">
+<meta name="description" content="With MikeLime's free web proxy, you can browse the internet freely without your school being able to see what you're doing! This web proxy will even unblock websites, and we have a wide selection of unblocked games.">
+<link rel="canonical" href="https://www.mikelime.ml">
+<meta charset="utf-8">
+<meta name="application-name" content="MikeLime's Web Proxy">
+<meta name="KeyWords" content="unblocked games, web proxy, how to unblock websites, website unblocker, unblock games, proxysite">
+<meta name="google-site-verification" content="Dq5W8bNH90DDy-tTImw5oFxSnS01ZYwENdl9Xp12WMo" />
+
+<script type="text/javascript" src="http://ylx-4.com/layer.php?section=General&pub=324864&ga=g&fp"></script>
+<style type="text/css">
+html body {
+	font-family: Arial,Helvetica,sans-serif;
+	font-size: 12px;
+}
+
+#container {
+	width:500px;
+	margin:0 auto;
+	margin-top:50px;
+}
+
+#error {
+	color:red;
+	font-weight:bold;
+}
+
+#frm {
+	padding:10px 15px;
+	
+	
+	border:1px solid #ffffff;
+	
+	-webkit-border-radius: 8px;
+	-moz-border-radius: 8px;
+	border-radius: 8px;
+}
+
+#footer {
+	text-align:center;
+	font-size:10px;
+	margin-top:35px;
+	clear:both;
+}
+</style>
+
+<style>
+
+body {
+  background:url("/wallpaper.jpg");
+
+  background-position: center center;
+  text-align:center;
+  background-repeat: fill;
+
+    background-attachment: fixed;
+
+        background-size: 100% 100%;
+}
+</style>
+
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+<style>
+
+a.button1{
+ display:inline-block;
+ padding:1.5em 3.0em;
+ border:0.1em solid #FFFFFF;
+ margin:0 0.3em 0.3em 0;
+ border-radius:0.12em;
+ box-sizing: border-box;
+ text-decoration:none;
+ font-family:'Roboto',sans-serif;
+ font-weight:300;
+ color:#FFFFFF;
+ text-align:center;
+ transition: all 0.2s;
+}
+a.button1:hover{
+ color:#000000;
+ background-color:#FFFFFF;
+}
+@media all and (max-width:30em){
+ a.button1{
+  display:block;
+  margin:0.4em auto;
+ }
+}
+</style>
+
+</head>
+
+<body>
+
+
+
+<div id="container" class="w3-animate-zoom">
+
+	
+
+<div style="text-align:center;">
+
+
+		
+		
+		
+		
+		<h1 style="color:#ffffff;">Welcome to MikeLime's Web Proxy!</h1>
+          														
+		<h3 style="color:#ffffff;">With MikeLime's free web proxy, you can browse the internet freely without your school being able to see what you're doing! This web proxy will even unblock websites, and we have a wide selection of unblocked games.</h3>
+	
+
+
+
+
+	
+	<?php if(isset($error_msg)){ ?>
+	
+	<div id="error">
+		<p><?php echo $error_msg; ?></p>
+	</div>
+	
+	<?php } ?>
+	
+	<div id="frm">
+	
+	<!-- I wouldn't touch this part -->
+	
+		<form action="index.php" method="post" style="margin-bottom:0;">
+			<input name="url" type="text" style="width:400px;" autocomplete="off" placeholder="http://" />
+			<input type="submit" value="Go" />
+		</form>
+		
+		<script type="text/javascript">
+			document.getElementsByName("url")[0].focus();
+		</script>
+		
+	<!-- [END] -->
+	
+	</div>
+	
+
+	<div style="text-align:center;">
+		<h2 style="color:#ffffff;">Just type a website in the bar above and click go!</h2>
+	 	
+
+<br>
+</br>
+
+<a href="https://sourlime.cf/index.php" class="button1">Proxy</a>
+<a href="https://sourlime.cf/games.html" class="button1">Games</a>
+<a href="https://sourlime.cf/movies.html" class="button1">Movies</a>
+<a href="https://sourlime.cf/chatbox.html" class="button1">Chatbox</a>
+
+<iframe data-aa='1023524' src='//ad.a-ads.com/1023524?size=200x90' scrolling='no' style='width:200px; height:90px; border:0px; padding:0;overflow:hidden' allowtransparency='true'></iframe>
+<br>
+</br>
+
+</div>
+
+
+</div>
+
+
+</body>
+</html>
